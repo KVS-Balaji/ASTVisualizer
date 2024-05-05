36990ed0adedb1f1868c9aa8bff87090cc03d985
jasmitha <jashjk82@gmail.com>
2024-05-05 17:23:47 +0530

    updated key

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,114 +3,15 @@ import * as d3 from "d3";
 import "./App.css";
 import { useRef, useEffect, useState } from "react";
 
-const data = {
-	name: "Program",
-	children: [
-		{
-			name: "=",
-			children: [
-				{
-					name: "a",
-				},
-				{
-					name: "+",
-					children: [
-						{
-							name: "10",
-						},
-						{
-							name: "5",
-						},
-					],
-				},
-			],
-		},
-		{
-			name: "If",
-			children: [
-				{
-					name: ["=="],
-					children: [
-						{
-							name: "a",
-						},
-						{
-							name: "15",
-						},
-					],
-				},
-				{
-					name: "If Body",
-					children: [
-						{
-							name: "print",
-							children: [
-								{
-									name: "a is 15",
-								},
-							],
-						},
-					],
-				},
-				{
-					name: "Else Body",
-					children: [
-						{
-							name: "If",
-							children: [
-								{
-									name: ["=="],
-									children: [
-										{
-											name: "a",
-										},
-										{
-											name: "10",
-										},
-									],
-								},
-								{
-									name: "If Body",
-									children: [
-										{
-											name: "print",
-											children: [
-												{
-													name: "a is 10",
-												},
-											],
-										},
-									],
-								},
-								{
-									name: "Else Body",
-									children: [
-										{
-											name: "print",
-											children: [
-												{
-													name: "a is some random value",
-												},
-											],
-										},
-									],
-								},
-							],
-						},
-					],
-				},
-			],
-		},
-	],
-};
-let astHierarchy = () => d3.hierarchy(data).sum(() => 1);
-let createAST = d3.tree().size([900, 300]);
-let AbstractSyntaxTree = createAST(astHierarchy());
-
 function App() {
 	const svgRef = useRef(null);
 	const [fileContent, setFileContent] = useState("");
 	const [astJson, setAstJson] = useState("");
+	
+
+	let astHierarchy = () => d3.hierarchy(astJson).sum(() => 1);
+	let createAST = d3.tree().size([900, 600]);
+	let AbstractSyntaxTree = createAST(astHierarchy());
 
 	useEffect(() => {
 		// Set svg size
@@ -153,24 +54,27 @@ function App() {
 			const astJsonData = await response.json();
 			setAstJson(astJsonData);
 			console.log(astJsonData);
+			astHierarchy = () => d3.hierarchy(astJsonData).sum(() => 1);
+			createAST = d3.tree().size([900, 600]);
+			AbstractSyntaxTree = createAST(astHierarchy());
 		} catch (error) {
 			console.error("Error fetching or processing AST:", error);
 		}
 	};
 
 	return (
-		<div id='appcontainer'>
-			<div id='heading'>
+		<div id="appcontainer">
+			<div id="heading">
 				<p>AST Visualizer</p>
 			</div>
-			<div id='maincontent'>
-				<div id='input-area'>
-					<div id='buttons'>
-						<div id='file-upload'>
+			<div id="maincontent">
+				<div id="input-area">
+					<div id="buttons">
+						<div id="file-upload">
 							<input
-								type='file'
-								id='file-input'
-								accept='.py'
+								type="file"
+								id="file-input"
+								accept=".py"
 								style={{ display: "none" }}
 							/>
 							<button
@@ -180,44 +84,52 @@ function App() {
 							</button>
 						</div>
 
-						<div id='run-btn'>
+						<div id="run-btn">
 							<button onClick={runCode}>Run</button>
 						</div>
 					</div>
-					<div id='file-content'>
+					<div id="file-content">
 						<textarea value={fileContent} readOnly></textarea>
 					</div>
 				</div>
-				<div id='tree-area'>
+				<div id="tree-area">
 					<svg ref={svgRef}>
-						<g transform='translate(0, 20)' id='ast'>
+						<g transform="translate(0, 20)" id="ast">
 							{astJson ? (
 								<>
 									{AbstractSyntaxTree.links().map(
-										({
-											source: { x: x1, y: y1 },
-											target: { x: x2, y: y2 },
-										}) => (
-											<line x1={x1} y1={y1} x2={x2} y2={y2} stroke='black' />
+										(
+											{ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } },
+											index
+										) => (
+											<line
+												key={`line-${index}`}
+												x1={x1}
+												y1={y1}
+												x2={x2}
+												y2={y2}
+												stroke="white"
+											/>
 										)
 									)}
 									{AbstractSyntaxTree.descendants().map(
-										({ x, y, data: { name } }) => (
-											<g key={name}>
+										({ x, y, data: { name } }, index) => (
+											<g key={`node-${index}`}>
+												
 												<circle
 													cx={x}
 													cy={y}
-													r={10}
-													fill='none'
-													stroke='white'
+													r={20}
+													fill="none"
+													stroke="white"
 												/>
 												<text
 													x={x}
 													y={y}
-													textAnchor='middle'
-													dominantBaseline='central'
-													fontSize='10px'
-													fill='white'
+													textAnchor="middle"
+													dominantBaseline="central"
+													fontSize="10px"
+													fill="white"
 												>
 													{name}
 												</text>
@@ -227,11 +139,11 @@ function App() {
 								</>
 							) : (
 								<text
-									x='450'
-									y='150'
-									textAnchor='middle'
-									fontSize='20px'
-									fill='white'
+									x="450"
+									y="150"
+									textAnchor="middle"
+									fontSize="20px"
+									fill="white"
 								>
 									No input data
 								</text>
