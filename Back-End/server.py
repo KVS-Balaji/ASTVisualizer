e49412bc672286c729c7880737a7f158ee8c874c
KVSB <kvsb414@gmail.com>
2024-05-06 09:41:18 +0530

    hopefully works on all systems

--- Back-End/server.py
+++ Back-End/server.py
@@ -11,20 +11,31 @@ CORS(app)
 def frontend():
     code = request.get_data().decode('utf-8')  
     decoded_code = base64.b64decode(code).decode('utf-8')
+    
+    cwd = os.getcwd()
+    if (cwd.__contains__("ASTVisualizer") == False):
+        cwd += "/ASTVisualizer"
+    if (cwd.__contains__("Front-End")):
+        cwd = cwd.removesuffix("/Front-End")
+        cwd += "/Back-End"
+    elif (cwd.__contains__("ASTVisualizer") and (cwd.__contains__("Back-End") == False)):
+        cwd += "/Back-End"
+    os.chdir(cwd)
 
-    with open('/home/kvsb/Documents/VSCode/Project Ideas/ASTVisualizer/Back-End/test.py', 'w') as f:
+    print(cwd)
+    with open(f'{cwd}/test.py', 'w') as f:
         f.write(decoded_code)
     subprocess.run(['python3', 'main.py'])
-    with open('/home/kvsb/Documents/VSCode/Project Ideas/ASTVisualizer/Back-End/ast.json', 'r') as f:
+    with open(f'{cwd}/ast.json', 'r') as f:
         ast_json_data = f.read()
-    # with open('/home/kvsb/Documents/VSCode/Project Ideas/ASTVisualizer/Front-End/src/App.jsx', 'r') as f:
-    #     print("hello")
-    return jsonify(ast_json_data)
+    
+    appjsx = open(f'{cwd.removesuffix("/Back-End")}/Front-End/src/App.jsx', 'w')
+    s1 = open(f'{cwd}/s1.txt', 'r')
+    s2 = open(f'{cwd}/s2.txt', 'r')
+    appcode = s1.read() + ast_json_data + s2.read()
+    appjsx.write(appcode)
 
-@app.route('/astjson')
-def send_json():
-    with open ('/home/kvsb/Documents/VSCode/Project Ideas/ASTVisualizer/Back-End/ast.json', 'r') as f:
-        return jsonify(f.read())
+    return "Done"
 
 
 if __name__ == '__main__':
